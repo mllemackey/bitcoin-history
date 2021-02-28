38e68cc8a8c0932ffaa5d2184211f5a6139021e3
mllemackey <mllemackey@gmail.com>
2021-03-01 00:34:55 +0100

    install chartjs and create chart component

---
+++ resources/js/components/Chart.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <canvas id="chart" width="400" height="400"></canvas>
+    </div>
+</template>
+
+<script>
+import Chartjs from 'chart.js';
+
+export default {
+    name: "Chart",
+    props: {
+        labels: Array,
+        dataProp: Array
+    },
+    methods: {
+        renderChart() {
+            new Chartjs(document.getElementById('chart').getContext('2d'), {
+                type: 'line',
+                data: {
+                    labels: this.labels,
+                    datasets: [{
+                        label: '',
+                        data: this.dataProp,
+                        lineTension: 0,
+                        fill: false,
+                        borderColor: 'rgb(27, 50, 128)',
+                        steppedLine: false,
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            },
+                            gridLines: {
+                                display: false
+                            }
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                display: false
+                            }
+                        }],
+                    }
+                }
+            });
+        }
+    },
+    mounted() {
+        this.renderChart();
+    }
+}
+</script>
+
+<style scoped>
+    #chart{
+        max-width: 500px;
+    }
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,10 +1,17 @@
 <template>
-
+    <div>
+        <h1>Bitcoin History</h1>
+        <Chart :data-prop="[20, 40, 10]" :labels="['2019-08-27', '2019-08-30', '2019-09-01']"></Chart>
+    </div>
 </template>
 
 <script>
+import Chart from "../components/Chart";
 export default {
-name: "App"
+    name: "App",
+    components: {
+        Chart
+    }
 }
 </script>
 
